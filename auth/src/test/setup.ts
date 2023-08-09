d2c32f276bedfa7fed8dba6a424f9a9ec6ba8c10
dpirvoiu <adisor.pdw@gmail.com>
2023-08-09 06:37:15 +0300

    chore: finish authentication tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,6 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
 
 let mongo: any;
@@ -25,3 +26,23 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
